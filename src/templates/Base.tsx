afebb5fa76960d76254b2fcf3e128e86976ba81a
Cameron <CameronCT@users.noreply.github.com>
2021-12-18 19:23:51 -0800

    fix: Base

--- src/templates/Base.tsx
+++ src/templates/Base.tsx
@@ -1,6 +1,10 @@
+import { redirect } from 'next/dist/server/api-utils';
 import React, { ReactNode, useEffect, useRef, useState } from 'react';
+import { GlobalHotKeys } from 'react-hotkeys';
 import PlaywireContainer from '../components/Advertisement/PlaywireContainer';
 import LoadContent from '../components/LoadContent';
+import Redirect from '../components/Uncategorized/Redirect';
+import useConfig from '../hooks/useConfig';
 import useGoogleAnalytics from '../hooks/useGoogleAnalytics';
 import Levelbar from '../layout/Levelbar';
 
@@ -17,16 +21,43 @@ type IMainProps = {
 };
 
 const Base = (props: IMainProps) => {
-
     useGoogleAnalytics();
 
     const timer = useRef<NodeJS.Timeout | null>(null);
-
+    const [ redirect, setRedirect ] = useState<string>('');
     const [ loaded, setLoaded ] = useState<boolean>(props.noAnimate === true);
+    const { shortcutHome, shortcutExit, shortcutPlayRandom, shortcutPlayQuotes, shortcutPlayDictionary } = useConfig();
 
-    const onTimerCalled = () => {
-        setLoaded(true);  
-    }
+    const onTimerCalled = () => setLoaded(true);
+
+    const redirectTo = (uri: string, isRefresh?: boolean, isBack?: boolean) => {
+        console.log('shortcut called', uri);
+        if (isRefresh) return window.location.reload();
+        if (isBack) return window.history.back();
+        if (!isRefresh && !isBack) {
+          setRedirect(uri);
+          setTimeout(() => {
+            setRedirect('');
+          }, 1);
+        }
+    };
+
+    const keyMap = {
+        REDIRECT_PLAY: shortcutExit.toLowerCase(),
+        REDIRECT_HOME: shortcutHome.toLowerCase(),
+        REDIRECT_PLAY_RANDOM: shortcutPlayRandom.toLowerCase(),
+        REDIRECT_PLAY_QUOTES: shortcutPlayQuotes.toLowerCase(),
+        REDIRECT_PLAY_DICTIONARY: shortcutPlayDictionary.toLowerCase(),
+    };
+
+    const handlers = {
+        REDIRECT_PLAY: () => redirectTo('/', false, false),
+        REDIRECT_HOME: () => redirectTo('/', false, false),
+        REDIRECT_PLAY_RANDOM: () => redirectTo('/play/random', false, false),
+        REDIRECT_PLAY_QUOTES: () => redirectTo('/play/regular', false, false),
+        REDIRECT_PLAY_DICTIONARY: () => redirectTo('/play/dictionary', false, false),
+    };
+
 
     useEffect(() => {
         if (!props.noAnimate && (typeof props.isLoaded === 'undefined' || (typeof props.isLoaded !== 'undefined' && props.isLoaded)))
@@ -38,8 +69,12 @@ const Base = (props: IMainProps) => {
         }
     }, [ props.isLoaded, props.noAnimate ]);
 
+    if (redirect)
+        return <Redirect to={redirect} />;
+        
     return props ? (
         <>
+            <GlobalHotKeys keyMap={keyMap} handlers={handlers} />
             {props.meta}
             {!props.noNav ? <Levelbar /> : ''}
             <main>
