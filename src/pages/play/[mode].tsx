e41d5e5466d7ae5256b6a9d693ab0befd1377601
Cameron <CameronCT@users.noreply.github.com>
2022-01-10 22:13:31 -0800

    chore: messing around with width values

--- src/pages/play/[mode].tsx
+++ src/pages/play/[mode].tsx
@@ -34,10 +34,10 @@ const Play = (props: IProps) => {
     return (
         <Base meta={<Meta title="Take your typing to the next level" />} ads={{ enableBottomRail: true }} isLoaded={loaded}>
             
-            <div className="absolute left-2 top-15">
+            <div className="absolute left-2 top-15 w-64">
                 <SidebarLong />
             </div>
-            <div className="absolute right-2 top-15">
+            <div className="absolute right-2 top-15 w-64">
                 <SidebarLong />
             </div>
 
