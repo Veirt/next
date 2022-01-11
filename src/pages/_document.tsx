8492c1b9553cb5fe32123d8070ce0e1f3c2fb937
Cameron <CameronCT@users.noreply.github.com>
2022-01-10 17:15:29 -0800

    fix: lint

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,7 @@
+// eslint-disable-next-line @next/next/no-document-import-in-page
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import React from 'react';
+// eslint-disable-next-line @next/next/no-script-in-document
 import Script from 'next/script'; 
  
 // Need to create a custom _document because i18n support is not compatible with `next export`.
