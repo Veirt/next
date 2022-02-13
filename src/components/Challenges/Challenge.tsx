e656d35c2b3da835ef84a9500ef0eb57e98693af
Cameron <CameronCT@users.noreply.github.com>
2022-02-13 00:34:46 -0800

    feat: challenge icons updated

--- src/components/Challenges/Challenge.tsx
+++ src/components/Challenges/Challenge.tsx
@@ -36,7 +36,7 @@ const Challenge:FC<PlayerChallengeData> = (props) => {
         <div>
             <div className="grid grid-cols-5 gap-4">
                 <div className="hidden 3xl:block col-span-full xl:col-span-1 my-auto">
-                    <img src={`/challenges/${challenge[0].type}.png`} alt={challenge[0].text} className={"block mx-auto transform scale-challenge"} />
+                    <img src={`/challenges/${challenge[0].type}.svg`} alt={challenge[0].text} className={"block mx-auto transform scale-110"} />
                 </div>
                 <div className="col-span-full 3xl:col-span-4 my-auto">
                     <div className="h4 text-orange-400" style={{ fontWeight: '700' }}>{mode[0].modeName}</div>
@@ -61,34 +61,6 @@ const Challenge:FC<PlayerChallengeData> = (props) => {
             </div>
         </div>
     ) : <></>;
-
-    /*
-    return (challenge[0] && mode[0]) ? (
-        <div>
-            <div className={"w-full sm:w-64 mx-auto py-1 relative z-10 font-bold bg-gray-800 shadow -mb-6 text-center text-orange-400 text-2xl rounded-2xl"}>
-                {mode[0].modeName}
-            </div>
-            <div className={"bg-gray-775 p-8 rounded-2xl"}>
-                <div className={"pt-8 pb-4 lg:pt-16 lg:pb-8"}>
-                    <img src={`/challenges/${challenge[0].type}.png`} alt={challenge[0].text} className={"w-11/12 sm:w-10/12 md:w-9/12 xl:w-auto block mx-auto transform scale-challenge"} />
-                </div>
-                <div className={"text-white text-base rounded-2xl text-center text-sm py-1.5 font-bold uppercase mb-6"}>
-                    {challenge[0].text}
-                </div>
-                <div className={"bg-gray-800 text-xl md:text-2xl xl:text-3xl text-orange-400 rounded-2xl text-center py-2 font-bold uppercase mb-6"}>
-                    {value > challenge[0].value ? challenge[0].value.toLocaleString() : value.toLocaleString()}<span className={"text-white"}>/{challenge[0].value.toLocaleString()}</span>
-                </div>
-                <button type={"button"} className={`${(!finished || finished === 2 || redeem) ? 'opacity-50 pointer-events-none' : 'opacity-100'} rounded-2xl focus:outline-none bg-orange-400 hover:bg-orange-500 text-orange-700 transition ease-in-out duration-300 font-bold text-lg xl:text-xl py-4 text-center block w-full`} onClick={redeemChallenges}>
-                    {(redeem || finished === 2) ? 'Redeemed' : 'Redeem'}
-                </button>
-            </div>
-            <div className={"w-4/5 mx-auto py-3 font-bold bg-gray-800 shadow -mt-4 text-center text-white text-xl xl:text-2xl rounded-2xl"}>
-                <FontAwesomeIcon icon={faCoins} className={"text-yellow-400 mr-1"} />
-                {challenge[0].rewards?.currency.toLocaleString()}
-            </div>
-        </div>
-    ) : <></>;
-    */
 }
 
 export default Challenge;
