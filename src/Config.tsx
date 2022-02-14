7b4bcbb4d622d495d2e98b769c9b5dff9589f41c
Cameron <CameronCT@users.noreply.github.com>
2022-02-14 14:30:23 -0800

    feat: v3 config

--- src/Config.tsx
+++ src/Config.tsx
@@ -7,15 +7,15 @@ const config = {
     defaultIcon: '/extras/avatar.jpg',
     defaultImage: '/extras/banner.jpg',
     webUrl: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : toggleStaging ? 'https://staging.Keymash' : 'https://keymash.io',
-    apiUrl: process.env.NODE_ENV === 'development' ? 'http://localhost:8080/api/v2' : toggleStaging ? 'https://apistaging.Keymash/api/v2' : 'https://api.Keymash/api/v2',
-    authUrl: process.env.NODE_ENV === 'development' ? 'http://localhost:8080/auth/v2' : toggleStaging ? 'https://apistaging.Keymash/auth/v2' : 'https://api.Keymash/auth/v2',
-    gameUrl: process.env.NODE_ENV === 'development' ? 'http://localhost:8080/data' : toggleStaging ? 'https://apistaging.Keymash/data' : 'https://api.Keymash/data',
-    oauthUrl: process.env.NODE_ENV === 'development' ? 'http://localhost:8080/auth/v2' : toggleStaging ? 'https://apistaging.Keymash/auth/v2' : 'https://api.Keymash/auth/v2',
+    apiUrl: process.env.NODE_ENV === 'development' ? 'http://localhost:8080/api/v2' : toggleStaging ? 'https://apistaging.keymash.io/api/v2' : 'https://api.keymash.io/api/v2',
+    authUrl: process.env.NODE_ENV === 'development' ? 'http://localhost:8080/auth/v2' : toggleStaging ? 'https://apistaging.keymash.io/auth/v2' : 'https://api.keymash.io/auth/v2',
+    gameUrl: process.env.NODE_ENV === 'development' ? 'http://localhost:8080/data' : toggleStaging ? 'https://apistaging.keymash.io/data' : 'https://api.keymash.io/data',
+    oauthUrl: process.env.NODE_ENV === 'development' ? 'http://localhost:8080/auth/v2' : toggleStaging ? 'https://apistaging.keymash.io/auth/v2' : 'https://api.keymash.io/auth/v2',
     cookieUrl: process.env.NODE_ENV === 'development' ? 'localhost' : '.Keymash',
     gameServer:
         process.env.NODE_ENV === 'development'
             ? { URL: 'http://localhost', Port: 2095, }
-            : { URL: toggleStaging ? 'https://wsstaging.Keymash' : 'https://us-east.Keymash', Port: null, },
+            : { URL: toggleStaging ? 'https://wsstaging.keymash.io' : 'https://us-east.keymash.io', Port: null, },
 };
 
 export default config;
