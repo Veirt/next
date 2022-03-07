19c1b0eabe7169090e4c26d142a696af3ef4323b
Cameron <CameronCT@users.noreply.github.com>
2022-03-07 14:30:36 -0800

    fix: lint

--- src/pages/news/v3-release.tsx
+++ src/pages/news/v3-release.tsx
@@ -1,17 +1,11 @@
-import {NewsletterData} from "../../types.client.mongo";
+
 import Base from "../../templates/Base";
 import { Meta } from "../../layout/Meta";
 import { GetServerSidePropsContext } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import ConfigService from "../../services/ConfigService";
 
-interface IProps {
-  slug: string
-  data: NewsletterData;
-  newsData: NewsletterData[];
-}
-
-const News = ({ data }: IProps) => {
+const News = () => {
     return (
         <Base meta={<Meta title={"v3 Released"} description={''} isThumbnail reverseTitle />} ads={{ enableBottomRail: true }}>
             <div className="container">
