9c882b253af99ebf1c3287b1ee87beca209b5cd1
Cameron <CameronCT@users.noreply.github.com>
2022-03-25 13:22:30 -0700

    fix?

--- src/components/Play/Queue.tsx
+++ src/components/Play/Queue.tsx
@@ -228,6 +228,8 @@ const Queue = (props: IProps) => {
         },
     ];
 
+    console.log('inQueue:', inQueue);
+
     return (
         <>
             {redirect && redirect !== '' && <Redirect to={redirect} />}
