f76d0a1d2e14045be440d2cf3d91ce5b65300976
Cameron <CameronCT@users.noreply.github.com>
2022-03-29 23:25:50 -0700

    chore: matchmaking re-created

--- src/components/Uncategorized/Matchmaking.tsx
+++ src/components/Uncategorized/Matchmaking.tsx
@@ -2,21 +2,23 @@ import {useEffect, useState} from 'react';
 import {usePlayerContext} from "../../contexts/Player.context";
 import Redirect from '../Uncategorized/Redirect';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheck, faCircleNotch, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faCheck, faCheckSquare, faCircleNotch, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { useRouter } from 'next/router';
 
 const Queue = () => {
 
     const router = useRouter();
 
-    const { inQueue, setInQueue, queueTimer, queueFound } = usePlayerContext();
+    const { inQueue, setInQueue, queueAccept, queuePending, queueTimer, queueFound, acceptQueue } = usePlayerContext();
     const [ redirect, setRedirect ] = useState('');
 
     useEffect(() => {
         let redirectInterval: NodeJS.Timeout | null = null;
 
-        if (queueFound) {
+        if (queuePending && !queueFound)
             (document.getElementById('MatchFound') as HTMLAudioElement)?.play();
+
+        if (queueFound) {
             if (router.pathname !== '/')
                 setRedirect('/');
 
@@ -32,7 +34,7 @@ const Queue = () => {
                 clearInterval(redirectInterval);
         }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [ router, redirect, queueFound ]);
+    }, [ router, redirect, queueFound, queuePending ]);
 
     const timerString = new Date(queueTimer * 1000).toISOString().substr(14, 5);
 
@@ -42,18 +44,22 @@ const Queue = () => {
             {inQueue && (
                 <div className="fixed top-14 xl:top-0 xl:mt-0 right-0 left-0 z-50">
                     <div className="flex justify-center"> 
-                        <div className={`${!queueFound ? 'bg-orange-300 border-orange-500 text-orange-900' : 'bg-green-300 border-green-500 text-green-900'} rounded-b-xl shadow-md w-72 px-4 py-2`}>
+                        <div className={`${!queuePending ? 'bg-orange-300 border-orange-500 text-orange-900' : 'bg-green-300 border-green-500 text-green-900'} rounded-b-xl shadow-md w-72 px-4 py-2`}>
                             <div className="flex justify-between">
                                 <div>
-                                    <FontAwesomeIcon icon={!queueFound ? faCircleNotch : faCheck} className="mr-2" spin={!queueFound} />
-                                    {!queueFound 
+                                    <FontAwesomeIcon icon={!queuePending ? faCircleNotch : faCheck} className="mr-2" spin={!queuePending} />
+                                    {!queuePending 
                                         ? <span className="font-semibold">Searching ({timerString})</span>
-                                        : <span className="font-semibold">Match Found</span>
+                                        : <span className="font-semibold">{queueFound ? 'Loading Match' : 'Waiting for Others'}</span>
                                     }
                                 </div>
-                                {!queueFound && (
+                                {(!queuePending && !queueAccept) ? (
                                     <button type="button" className="focus:outline-none text-orange-900 hover:opacity-70 transition ease-in-out duration-300" onClick={() => setInQueue(false)}>
                                         <FontAwesomeIcon icon={faTimes} className="mr-2" />
+                                    </button>
+                                ) : !queueAccept && (
+                                    <button type="button" className="focus:outline-none text-green-900 hover:opacity-70 transition ease-in-out duration-300" onClick={acceptQueue}>
+                                        <FontAwesomeIcon icon={faCheckSquare} className="mr-2" />
                                     </button>
                                 )}
                             </div>
